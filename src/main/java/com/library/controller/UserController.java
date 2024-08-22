40f888230672596d67a59b3824ed5d59d307c5fb
Ibrahim Musa <ibrahimmusa@imusa-sg.local>
2024-08-22 09:07:18 +0100

    added entity, services, and controllers for the library management system

---
+++ src/main/java/com/library/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.library.controller;
+
+import com.library.entity.User;
+import com.library.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
---
+++ src/main/java/com/library/service/UserService.java
@@ -0,0 +1,25 @@
+package com.library.service;
+
+import com.library.entity.User;
+import com.library.repo.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
